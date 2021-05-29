be6fa46bfbf00ee91153291b5ac8075d9f432e73
storm-sergey <st.sergey.a@gmail.com>
2021-05-29 13:08:07 +0300

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Main from "./Main/Main";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Main />
+      </header>
+    </div>
+  );
+}
+
+export default App;
